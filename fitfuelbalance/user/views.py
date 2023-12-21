7c52e09a3b89e11b9d5fc363137d1d34ce0a0a85
Monte-10 <61688940+Monte-10@users.noreply.github.com>
2023-12-21 23:03:55 +0100

    Servicio de login terminado

--- fitfuelbalance/user/views.py
+++ fitfuelbalance/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
 from .forms import *
 
 
@@ -10,3 +11,11 @@ class SingUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+    
+    def form_valid(self, form):
+        valid = super(SingUpView, self).form_valid(form)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return valid
