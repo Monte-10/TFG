a8275b1e6b30025a3aeb9a178051e547bdab9834
Monte-10 <61688940+Monte-10@users.noreply.github.com>
2024-05-26 15:21:28 +0200

    Vuelta de prod

--- fitfuelbalance/fitfuelbalance/settings.py
+++ fitfuelbalance/fitfuelbalance/settings.py
@@ -125,7 +125,7 @@ if not DEBUG:
 
 AUTH_USER_MODEL = 'user.CustomUser'
 
-LOGIN_REDIRECT_URL = "https://tfg-691w.onrender.com"
+LOGIN_REDIRECT_URL = "http://127.0.0.1:3000"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
